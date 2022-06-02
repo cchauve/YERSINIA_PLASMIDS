5ae85ee22e3c104662b65aab74d8cd777f962fda
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2022-06-02 12:46:31 -0700

    Assembling scripts

---
+++ assemblies/fmicb.2021.664665.sh
@@ -0,0 +1,5 @@
+sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies/single-sample.pl -r yent SAMEA7965450 ERR5265261 NA
+sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies/single-sample.pl -r yent SAMEA7965451 ERR5265260 NA
+sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies/single-sample.pl -r yent SAMEA7965452 ERR5265259 NA
+sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies/single-sample.pl -r yent SAMEA7965453 ERR5265258 NA
+sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies/single-sample.pl -r yent SAMEA7965454 ERR5265257 NA
