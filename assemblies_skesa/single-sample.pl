02329b76331cce9c729d9fbaa1d926a291da0719
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-06-04 09:52:42 -0700

    Adding k-mers count

--- assemblies_skesa/single-sample.pl
+++ assemblies_skesa/single-sample.pl
@@ -41,6 +41,7 @@ unless (-e "short_2.fastq.gz" && $Options{'r'}) {
 unless (-e "$dir/short.gfa" && $Options{'r'}) {
     my_run("/home/chauvec/projects/ctb-chauvec/PLASMIDS/tools/SKESA/skesa --reads short_1.fastq.gz,short_2.fastq.gz --min_contig 100 --contigs_out $dir/short.fasta 2>&1 >>$dir/contigs.log");
     my_run("/home/chauvec/projects/ctb-chauvec/PLASMIDS/tools/SKESA/gfa_connector --reads short_1.fastq.gz,short_2.fastq.gz --contigs $dir/short.fasta --gfa $dir/short.gfa --csv $dir/short.csv 2>&1 >>$dir/graph.log");
+    my_run("/home/chauvec/projects/ctb-chauvec/PLASMIDS/tools/SKESA/kmercounter --reads short_1.fastq.gz,short_2.fastq.gz --text_out $dir/short.kmers --hist $dir/short.hist 2>&1 >>$dir/kmers.log");
 }
 
 #upracsiposebe
