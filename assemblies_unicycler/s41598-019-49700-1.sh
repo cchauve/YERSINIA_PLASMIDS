ea4e0236f941a37c2f9c94b9a38a40cc159c027e
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-06-06 16:33:09 -0700

    Assembly scripts

--- assemblies_unicycler/s41598-019-49700-1.sh
+++ assemblies_unicycler/s41598-019-49700-1.sh
@@ -1,2 +1,2 @@
-sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies/single-sample.pl -r bant SAMN10984861 SRR8608856 NA
-sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies/single-sample.pl -r ypest SAMN10984860 SRR8608858 NA
+sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies_unicycler/single-sample.pl -r bant SAMN10984861 SRR8608856 NA
+sbatch --time=1-0 -c 8 --mem=30G --account=def-chauvec /home/chauvec/projects/ctb-chauvec/PLASMIDS/YERSINIA_PLASMIDS/assemblies_unicycler/single-sample.pl -r ypest SAMN10984860 SRR8608858 NA
